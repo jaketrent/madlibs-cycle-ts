f44fbd10d970036ac8ab42600b55fa2d971a3c5d
jaketrent <trent.jake@gmail.com>
2016-06-26 15:30:29 -0600

    capture story input

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { makeDOMDriver, DOMSource, div, VNode } from '@cycle/dom'
+import { makeDOMDriver, DOMSource, div, textarea, VNode } from '@cycle/dom'
 import { run } from '@cycle/xstream-run'
 import xs, { Stream } from 'xstream'
 
@@ -11,10 +11,42 @@ interface Drivers {
 interface Sinks {
   DOM: Stream<VNode>
 }
+interface ViewState {
+  story$: Stream<string>
+}
+interface Event {
+  target: EventTarget
+}
+interface EventTarget {
+  value: string
+}
+
+function intent(DOMSource: DOMSource) {
+  const story$ = DOMSource
+    .select('.story')
+    .events('keyup')
+    .map((evt: Event) => evt.target.value)
+    .startWith('')
+
+  return { story$ }
+}
+
+function model(story$: Stream<string>): ViewState {
+  return { story$ }
+}
+
+function view(state: ViewState): Stream<VNode> {
+  return state.story$.map(story =>
+    textarea('.story', story)
+  )
+}
 
 function main(sources: Sources): Sinks {
+  const { story$ } = intent(sources.DOM)
+  const state$ = model(story$)
+  const vtree$ = view(state$)
   return {
-    DOM: xs.of(div('Madlibs'))
+    DOM: vtree$
   }
 }
 
