87720b2c545564f4841722d43ec068d082e67e3f
jaketrent <trent.jake@gmail.com>
2016-06-29 09:24:44 -0600

    mv index.html

--- dev-server.js
+++ dev-server.js
@@ -1,5 +1,6 @@
 const koa = require('koa')
 const mount = require('koa-mount')
+const path = require('path')
 const static = require('koa-static')
 const webpackDevMiddleware = require('koa-webpack-dev-middleware')
 const webpack = require('webpack')
@@ -10,7 +11,7 @@ const app = koa()
 const port = process.env.PORT || 3000
 
 app.use(mount('/static', webpackDevMiddleware(webpack(config))))
-app.use(static(__dirname))
+app.use(static(path.join(__dirname, 'src')))
 
 app.listen(port)
 console.log('Listening on port ' + port)
